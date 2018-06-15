454887adcafb2779c2d90464ffea4ed0db17fd80
Gajesh Bhat <gajeshbht@gmail.com>
2018-06-15 22:51:04 +0530

    Added Js audio player

--- Podcast/urls.py
+++ Podcast/urls.py
@@ -4,7 +4,7 @@ from Podcast.views import *
 urlpatterns = [
 url(r'^create/$',CreatePodcastView,name="Create Podcast Page"),
 url(r'^archives/$',PodcastListView,name="Podcast List Page"),
-url(r'^archives/(?P<slug>[-\w]+)/$',PodcastDetailView,name='Podcast Detail Page'), #Podcast Details page
+url(r'^archives/(?P<slug>[-\w]+)/$',PodcastDetailView,name='Podcast Detail Page'),
 url(r'^archives/(?P<slug>[-\w]+)/edit/$',PodcastEditView,name="Podcast Edit Info"),
 url(r'^archives/(?P<slug>[-\w]+)/delete/$',PodcastDeleteView,name="Delete Podcast"),
 url(r'^archives/(?P<slug>[-\w]+)/newepisode/$',CreateEpisodeView,name="Create an Episode"),
